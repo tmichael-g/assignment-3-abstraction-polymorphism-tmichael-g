2c2d26c090fca93d89411e89919a0f702e45b3b5
tmichael-g <t.michael.gt@gmail.com>
2026-04-05 13:39:39 +0300

    Implement SmartLight class extending SmartDevice

--- src/main/java/com/jju/SmartLight.java
+++ src/main/java/com/jju/SmartLight.java
@@ -1,1 +1,12 @@
 
+public class SmartLight extends SmartDevice {
+    
+    public SmartLight(String name) {
+        super(name); // Pass the name to the abstract parent constructor
+    }
+
+    @Override
+    public void activate() {
+        System.out.println(getName() + " is now ON at 100% brightness.");
+    }
+}
