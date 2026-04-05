0b84b2b511e920f2567f34e03a4b3583f6b2f061
Tesfamichael-G <tesfamichael.gegziabher@aait.edu.et>
2026-04-05 12:58:24 +0300

    Update App.java

--- src/main/java/com/jju/App.java
+++ src/main/java/com/jju/App.java
@@ -1,16 +1,20 @@
+package com.jju;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<SmartDevice> home = new ArrayList<>();
+        // Polymorphic List: Holds different types of devices under one parent type
+        List<SmartDevice> home = new ArrayList<>();
 
         home.add(new SmartLight("Living Room Lamp"));
         home.add(new SmartThermostat("Hallway Nest"));
 
         System.out.println("--- Activating Smart Home ---");
+
+        // Polymorphic Loop: The 'device' variable takes many forms
         for (SmartDevice device : home) {
-            System.out.print(device.getName() + ": ");
-            device.activate(); // Polymorphism happens here!
+            device.activate(); 
         }
     }
 }
