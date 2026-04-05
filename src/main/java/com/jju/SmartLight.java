d41f3896bec5f109059e1debc17bf3121586baee
tmichael-g <t.michael.gt@gmail.com>
2026-04-05 13:40:47 +0300

    Update SmartLight.java

--- src/main/java/com/jju/SmartLight.java
+++ src/main/java/com/jju/SmartLight.java
@@ -1,1 +1,12 @@
 
+public class SmartThermostat extends SmartDevice {
+
+    public SmartThermostat(String name) {
+        super(name);
+    }
+
+    @Override
+    public void activate() {
+        System.out.println(getName() + " is setting temperature to 72 degrees.");
+    }
+}
