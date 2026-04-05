fc8ca7acecef1e7764072edc9a2de5ea471f9a78
tmichael-g <t.michael.gt@gmail.com>
2026-04-05 13:42:07 +0300

    Add SmartThermostat class extending SmartDevice

--- src/main/java/com/jju/SmartLight.java
+++ src/main/java/com/jju/SmartLight.java
@@ -1,1 +1,13 @@
+package com.jju;
 
+public class SmartThermostat extends SmartDevice {
+
+    public SmartThermostat(String name) {
+        super(name);
+    }
+
+    @Override
+    public void activate() {
+        System.out.println(getName() + " is setting temperature to 72 degrees.");
+    }
+}
